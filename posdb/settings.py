607bcbfa42d20af0469d9847d0b334e48a77bde0
herrotha6712-sketch <herrotha6712@gmail.com>
2026-05-15 16:11:42 +0700

    Update settings.py

--- posdb/settings.py
+++ posdb/settings.py
@@ -12,11 +12,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '2006d105.pythonanywhere.com',
-    '127.0.0.1',
-    'localhost',
-]
+ALLOWED_HOSTS = ['2006d105.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
